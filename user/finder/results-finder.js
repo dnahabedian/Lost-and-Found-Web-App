f98aeab2eff8bd1e462bd544a3bd9c0a7b891617
Daron Nahabedian <nahabediand@gmail.com>
2022-10-10 15:25:41 -0700

    Create results-finder.js

---
+++ user/finder/results-finder.js
@@ -0,0 +1,9 @@
+let logo = document.getElementById("ucd-logo");
+logo.addEventListener("click", ()=>{
+    window.location.href = "https://hammerhead-glittery-ceres.glitch.me/user/views/splashscreen.html";
+});
+
+let editButton = document.getElementById("edit-search-button");
+editButton.addEventListener("click", ()=>{
+    window.location.href = "https://hammerhead-glittery-ceres.glitch.me/user/views/seeker-mapscreen.html";
+})
