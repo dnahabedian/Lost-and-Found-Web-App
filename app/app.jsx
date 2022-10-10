1e43abdf7fbb687354fbbfee49764e61d08f6c50
Daron Nahabedian <nahabediand@gmail.com>
2022-10-10 15:20:30 -0700

    Create app.jsx

---
+++ app/app.jsx
@@ -0,0 +1,6 @@
+const React = require('react');
+const ReactDOM = require("react-dom");
+
+const Results = require("./Results");
+
+ReactDOM.render(<Results></Results>, document.getElementById("results-list"));
